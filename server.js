b2bc49fc14eab177eeb66d2968a437729062aa21
tirtha1 <37182361+tirtha1@users.noreply.github.com>
2018-03-17 08:18:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article1', function (req, res) {
+  res.send('article 1 req and handeled here');
+});
+
+app.get('/article1', function (req, res) {
+  res.send('article 1 req and handeled here');
+});
+
+app.get('/article1', function (req, res) {
+  res.send('article 1 req and handeled here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
